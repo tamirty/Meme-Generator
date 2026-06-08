cffae3ef1c021693c363769a78f2ffeb2a199f61
tamirty <tamirty@gmail.com>
2026-06-08 20:13:23 +0300

    Adding basic canvas function

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,46 @@
 'use strict'
+
+var gElCanvas
+var gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    // addListeners()
+    renderMeme()
+}
+
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+function drawImg() {
+    const elImg = new Image()  
+    elImg.src = 'imgs/3.jpg'
+    console.log(elImg);
+    
+    elImg.onload = () => coverCanvasWithImg(elImg)
+}
+
+function coverCanvasWithImg(elImg) {
+    gElCanvas.height = (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function renderMeme() {
+    drawImg()
+}
