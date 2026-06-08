73d6ecef8452951369729772d75c67eac8d7cda7
tamirty <tamirty@gmail.com>
2026-06-08 18:45:33 +0300

    Init repo

---
+++ js/services/util.service.js
@@ -0,0 +1,46 @@
+'use strict'
+
+function makeId(length = 6) {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var txt = ''
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return txt
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive 
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
+}
+
+function getRandomColor() {
+    const hexDigits = '0123456789ABCDEF'
+    let color = '#'
+
+    for (var i = 0; i < 6; i++){
+        color += pickRandom(hexDigits)
+    }
+    return color
+}
+
+function pickRandom(arr) {
+    const idx = getRandomInt(0, arr.length)
+    return arr[idx]
+}
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
