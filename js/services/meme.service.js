05318bb8d06566a9e8756ea8353c45db5d68ed91
tamirty <tamirty@gmail.com>
2026-06-15 21:35:22 +0300

    app ver 1 commit

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -5,21 +5,21 @@ const STORAGE_KEY = 'memeDB'
 var gSavedMemes = loadFromStorage(STORAGE_KEY) || []
 
 var gImgs = [
-    { id: 2, url: 'imgs/2.jpg', keywords: ['funny', 'cat'] },
-    { id: 3, url: 'imgs/3.jpg', keywords: ['funny', 'cat'] },
-    { id: 4, url: 'imgs/4.jpg', keywords: ['funny', 'cat'] },
-    { id: 5, url: 'imgs/5.jpg', keywords: ['funny', 'cat'] },
-    { id: 6, url: 'imgs/6.jpg', keywords: ['funny', 'cat'] },
-    { id: 7, url: 'imgs/7.jpg', keywords: ['funny', 'cat'] },
-    { id: 8, url: 'imgs/8.jpg', keywords: ['funny', 'cat'] },
-    { id: 9, url: 'imgs/9.jpg', keywords: ['funny', 'cat'] },
-    { id: 11, url: 'imgs/11.jpg', keywords: ['funny', 'cat'] },
-    { id: 12, url: 'imgs/12.jpg', keywords: ['funny', 'cat'] },
-    { id: 13, url: 'imgs/13.jpg', keywords: ['funny', 'cat'] },
-    { id: 14, url: 'imgs/14.jpg', keywords: ['funny', 'cat'] },
-    { id: 15, url: 'imgs/15.jpg', keywords: ['funny', 'cat'] },
-    { id: 16, url: 'imgs/16.jpg', keywords: ['funny', 'cat'] },
-    { id: 18, url: 'imgs/18.jpg', keywords: ['funny', 'cat'] },
+    { id: 1, url: 'imgs/2.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: 'imgs/3.jpg', keywords: ['funny', 'cat'] },
+    { id: 3, url: 'imgs/4.jpg', keywords: ['funny', 'cat'] },
+    { id: 4, url: 'imgs/5.jpg', keywords: ['funny', 'cat'] },
+    { id: 5, url: 'imgs/6.jpg', keywords: ['funny', 'cat'] },
+    { id: 6, url: 'imgs/7.jpg', keywords: ['funny', 'cat'] },
+    { id: 7, url: 'imgs/8.jpg', keywords: ['funny', 'cat'] },
+    { id: 8, url: 'imgs/9.jpg', keywords: ['funny', 'cat'] },
+    { id: 9, url: 'imgs/11.jpg', keywords: ['funny', 'cat'] },
+    { id: 10, url: 'imgs/12.jpg', keywords: ['funny', 'cat'] },
+    { id: 11, url: 'imgs/13.jpg', keywords: ['funny', 'cat'] },
+    { id: 12, url: 'imgs/14.jpg', keywords: ['funny', 'cat'] },
+    { id: 13, url: 'imgs/15.jpg', keywords: ['funny', 'cat'] },
+    { id: 14, url: 'imgs/16.jpg', keywords: ['funny', 'cat'] },
+    { id: 15, url: 'imgs/18.jpg', keywords: ['funny', 'cat'] },
 ]
 
 var gMeme = {
